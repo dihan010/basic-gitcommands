82e2dadef28c4d29b3b0279fcc0bff02bb4e3818
dihan010 <dihan.stu2019@juniv.edu>
2022-11-06 00:48:26 +0600

    Create math.js

---
+++ math.js
@@ -0,0 +1,17 @@
+var unitPrice=100;
+var quantity=20;
+var totalPrice=unitPrice*quantity;
+// console.log(totalPrice);
+
+var x=30;
+var y=20;
+var z=x+y;
+console.log("sum ===>",z);
+
+var studentAge1=29;
+var studentAge2=25;
+var difference=studentAge1-studentAge2;
+console.log("difference ===>",difference);
+
+var remainder=studentAge1%studentAge2
+console.log("remainder===>",remainder)
