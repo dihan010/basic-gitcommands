c15a5e53b4c6fe8eb6c8c796a53eb431f179b3f7
dihan010 <dihan.stu2019@juniv.edu>
2022-11-06 11:33:08 +0600

    Create shorthand.js

---
+++ shorthand.js
@@ -0,0 +1,34 @@
+var price1=10;
+price1=price1+10
+console.log(price1);
+
+//shorthand
+var price1=10;
+price1+=10
+console.log(price1);
+
+var price1=10;
+price1-=10
+console.log(price1);
+
+var price1=10;
+price1*=10
+console.log(price1);
+
+var price1=10;
+price1/=10
+console.log(price1);
+
+var price1=10;
+price1%=10
+console.log(price1);
+
+//increment of +1
+var price1=10;
+price1++
+console.log(price1);
+
+//decrease of 1
+var price1=10;
+price1--
+console.log(price1);
