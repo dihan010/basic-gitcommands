79510089b0ba02fe924287932afca67ac772a1ce
dihan010 <dihan.stu2019@juniv.edu>
2022-11-06 12:28:34 +0600

    Create precedence.js

---
+++ precedence.js
@@ -0,0 +1,14 @@
+//operator precedence
+var now=2022;
+var ageLimon=now-1992;
+var ageDihan=now-1998;
+console.log(now-1992>now-1998);
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#:~:text=omit%20the%20associativity.-,Precedence,-Operator%20type
+
+var x,y
+x=y=25-10-5;
+console.log(x,y);
+
+var averageAge=(ageDihan+ageLimon)/2
+console.log(ageDihan,ageLimon,averageAge)
