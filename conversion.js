c965ccc701ed734868e893601d18ea7b6392c4a0
dihan010 <dihan.stu2019@juniv.edu>
2022-11-06 11:25:34 +0600

    Create conversion.js

---
+++ conversion.js
@@ -0,0 +1,10 @@
+var price1='40';
+var price2='35';
+var price1Number=parseInt(price1);
+var price2Number=parseInt(price2);
+var totalPrice=price1Number+price2Number;
+console.log(totalPrice);
+
+var gpa='3.92';
+var gpaNumber=parseFloat(gpa);
+console.log(gpaNumber);
