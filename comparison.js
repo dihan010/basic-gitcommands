458c90f95f4389dd812045bf35a8813ce3c52a15
dihan010 <dihan.stu2019@juniv.edu>
2022-11-06 11:59:40 +0600

    new files

---
+++ comparison.js
@@ -0,0 +1,12 @@
+// comparison operators > >= < <= === == != !==
+
+var ageLimon=30;
+var ageDihan=24;
+console.log(ageLimon>ageDihan);
+console.log(ageLimon>=ageDihan);
+console.log(ageLimon<ageDihan);
+console.log(ageLimon<=ageDihan);
+console.log(ageLimon===ageDihan); //strict equality
+console.log(ageLimon==ageDihan); //loose equality
+console.log(ageLimon!=ageDihan); //loose inequality
+console.log(ageLimon!==ageDihan); //strict inequality
---
+++ logicalOperators.js
@@ -0,0 +1,8 @@
+//logical operator && || !
+
+var hasDriversLicense=true;
+var hasGoodVision=true;
+var isTired=false;
+
+console.log(hasDriversLicense && hasGoodVision && isTired);
+console.log(hasDriversLicense || hasGoodVision || isTired);
